1d549c338cbfe87a68479d3f9a602d1da5a8ac0e
sapna maurya <io5.sapnamaurya@gmail.com>
2024-01-26 14:25:04 +0530

    motion

---
+++ Spotify Clone/script.js
@@ -0,0 +1,24 @@
+console.log("welcome to spotify")
+let songList = [
+    { songName: "Tum Bin", filePath: "songList/tum bin.mp3", coverPath: "/list.webp" },
+    { songName: "Tum Bin", filePath: "songList/tum bin.mp3", coverPath: "/list.webp" },
+    { songName: "Tum Bin", filePath: "songList/tum bin.mp3", coverPath: "/list.webp" },
+    ,
+    { songName: "Tum Bin", filePath: "songList/tum bin.mp3", coverPath: "/list.webp" }
+    ,
+    { songName: "Tum Bin", filePath: "songList/tum bin.mp3", coverPath: "/list.webp" }
+    ,
+    { songName: "Tum Bin", filePath: "songList/tum bin.mp3", coverPath: "/list.webp" }
+    ,
+    { songName: "Tum Bin", filePath: "songList/tum bin.mp3", coverPath: "/list.webp" }
+    ,
+    { songName: "Tum Bin", filePath: "songList/tum bin.mp3", coverPath: "/list.webp" }
+    ,
+
+
+
+
+
+]
+
+let audioElement = new Audio('tum bin.mp3')
